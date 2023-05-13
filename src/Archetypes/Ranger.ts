b14199561da914f3ea8342769a0f83cc85a3b1c3
NettoJM <nettojm86@gmail.com>
2023-05-13 20:47:26 -0300

    Add "Ranger" class extending "Archetype" abstract class

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Ranger extends Archetype {
+  protected _energyType: EnergyType;
+  private static _instancesCount = 0;
+
+  constructor(protected _name: string) {
+    super(_name);
+    this._energyType = 'stamina';
+    Ranger._instancesCount += 1;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return Ranger._instancesCount;
+  }
+}
