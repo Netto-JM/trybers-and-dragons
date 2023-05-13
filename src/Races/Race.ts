dbed64b675e6a0500b1fac67d4e8e3ea4bf144b8
NettoJM <nettojm86@gmail.com>
2023-05-13 19:01:17 -0300

    Modified properties visibility

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,5 +1,5 @@
 export default abstract class Race {
-  constructor(private _name: string, private _dexterity: number) {}
+  constructor(protected _name: string, protected _dexterity: number) {}
 
   public get name(): string {
     return this._name;
