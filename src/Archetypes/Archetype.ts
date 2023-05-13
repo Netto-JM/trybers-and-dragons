eb20a126a9d3c2d7927052e272be33f26c4027d1
NettoJM <nettojm86@gmail.com>
2023-05-13 19:56:42 -0300

    Add "Achetype" abstract class

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,30 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  protected _special: number;
+  protected _cost: number;
+
+  constructor(protected _name: string) {
+    this._name = _name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
+  }
+
+  public abstract get energyType(): EnergyType;
+
+  public static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
