77a3f0ce450ca7b741210b814899921bed09d29c
NettoJM <nettojm86@gmail.com>
2023-05-13 20:45:12 -0300

    Add "Mage" class extending "Archetype" abstract class

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  protected _energyType: EnergyType;
+  private static _instancesCount = 0;
+
+  constructor(protected _name: string) {
+    super(_name);
+    this._energyType = 'mana';
+    Mage._instancesCount += 1;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return Mage._instancesCount;
+  }
+}
